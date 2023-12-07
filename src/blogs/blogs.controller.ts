ebda7e33e1105f2e74028037f8aedf1e97f5839a
Kirill Makarov <kirill.makarov@visitech.ru>
2023-12-07 17:56:39 +0300

    feat: add methods post, get by id

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,13 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
+const counter = () => {
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  };
+};
+const idIncremenator = counter();
+const blogsCollection: Array<CreateBlogInputDto & { id: number }> = [];
 @Controller('blogs')
 export class BlogsController {
   @Get()
   async getBlog() {
-    return {
-      id: new Date().toISOString(),
-      blogName: 'blog',
-      description: 'description',
+    return blogsCollection;
+  }
+  @Get(':id')
+  async getBlogById(@Param() params: { id: string }) {
+    return blogsCollection.filter((item) => item.id === +params.id);
+  }
+  @Post()
+  async createBlog(@Body() inputModel: CreateBlogInputDto) {
+    const newBlog = {
+      id: idIncremenator(),
+      blogName: inputModel.blogName,
+      description: inputModel.description,
     };
+    blogsCollection.push(newBlog);
+    return newBlog;
   }
 }
+
+type CreateBlogInputDto = {
+  blogName: string;
+  description: string;
+};
