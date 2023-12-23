f546342deebac579b4f6e5b9d45b1dba9150c525
Kirill Makarov <kirill.makarov@visitech.ru>
2023-12-23 21:46:56 +0300

    test: add test

--- test/blogs.crud.e2e-spec.ts
+++ test/blogs.crud.e2e-spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
+import request, { SuperAgentTest } from 'supertest';
 import { AppModule } from './../src/app.module';
+import { HttpStatus } from '../src/common/types';
+import { RouterPaths } from '../src/application/utils/router-paths';
 
 describe('/blogs tests', () => {
     let app: INestApplication;
+    let http: SuperAgentTest;
 
     beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,12 +16,19 @@ describe('/blogs tests', () => {
 
         app = moduleFixture.createNestApplication();
         await app.init();
+
+        await request(app.getHttpServer())
+            .delete(RouterPaths.testing);
+        http = app.getHttpServer();
     });
 
-    beforeAll(async() => {
-    return request(app.getHttpServer())
-      .delete('/testing/all-data')
-      .expect(200)
-      .expect('Hello World!');
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it(`should return an object with 0 totalCount`, async () => {
+        await request(http)
+            .get(RouterPaths.blogs)
+            .expect(HttpStatus.OK_200, {pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: []});
     });
 });
