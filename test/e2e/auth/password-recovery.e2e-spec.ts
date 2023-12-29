d9b4c77ba5a58f64be3b72a468aa0318f8c0b5c7
Kirill Makarov <kirill.makarov@visitech.ru>
2023-12-29 10:58:58 +0300

    fix[password recovery test]: add mongoose User model in test environment

--- test/e2e/auth/password-recovery.e2e-spec.ts
+++ test/e2e/auth/password-recovery.e2e-spec.ts
@@ -1,7 +1,12 @@
 import { HttpStatus } from '../../../src/application/types/types';
 import { JwtService } from '../../../src/application/adapters/jwt-service';
 import { UserViewModel } from '../../../src/features/users/types/user-view-model';
-import { PasswordRecoveryType, UserDocument } from '../../../src/features/users/03-domain/user-db-model';
+import {
+    PasswordRecoveryType,
+    User,
+    UserDocument,
+    UserSchema,
+} from '../../../src/features/users/03-domain/user-db-model';
 import { CreateUserInputModel } from '../../../src/features/users/types/create-input-user-model';
 import { usersTestManager } from '../../utils/usersTestManager';
 import { RouterPaths } from '../../../src/application/types/router-paths';
@@ -11,6 +16,7 @@ import { INestApplication } from '@nestjs/common';
 import request, { SuperAgentTest } from 'supertest';
 import { authBasicHeader } from '../../utils/test_utilities';
 import { AppModule } from '../../../src/app.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 // const emailAdapter = {
 //     async sendEmail(email: string, subject: string, message: string): Promise<boolean> {
@@ -30,7 +36,15 @@ describe('testing password recovery', () => {
     beforeEach(async () => {
         const moduleRef = await Test.createTestingModule({
             // controllers: [],
-            imports: [AppModule],
+            imports: [
+                AppModule,
+                MongooseModule.forFeature([
+                    {
+                        name: User.name,
+                        schema: UserSchema,
+                    },
+                ]),
+            ],
             providers: [JwtService, UsersRepository],
         }).compile();
 
@@ -43,8 +57,8 @@ describe('testing password recovery', () => {
         await request(httpServer).delete(RouterPaths.testing);
     });
     // изначальные credentials
-    const email: string = "email123@mail.com";
-    const passwordBeforeChanging: string = "password123";
+    const email: string = 'email123@mail.com';
+    const passwordBeforeChanging: string = 'password123';
     // сюда сохраним юзера
     let user: UserViewModel | null = null;
     // сюда сохраним объект с кодом восстановления
@@ -55,29 +69,32 @@ describe('testing password recovery', () => {
     beforeAll(async () => {
         // Создаем юзера
         const userData: CreateUserInputModel = {
-            login: "login1",
+            login: 'login1',
             password: passwordBeforeChanging,
-            email: email
-        }
+            email: email,
+        };
 
-        const {createdUser} = await usersTestManager.createUser(userData, HttpStatus.CREATED_201, authBasicHeader)
+        const { createdUser } = await usersTestManager.createUser(
+            userData,
+            HttpStatus.CREATED_201,
+            authBasicHeader,
+        );
         user = createdUser;
     });
 
     it('Check that necessary support objects have been successfully created', async () => {
         expect(user).not.toBeNull();
-    })
+    });
 
-    it('should return status 204 even if such email doesnt exist; status 204;', async() => {
-
+    it('should return status 204 even if such email doesnt exist; status 204;', async () => {
         const data = {
-            'email': 'unexistingEmailAddress@jopa.com'
-        }
+            email: 'unexistingEmailAddress@jopa.com',
+        };
 
         await request(httpServer)
             .post(`${RouterPaths.auth}/password-recovery`)
             .send(data)
-            .expect(HttpStatus.NO_CONTENT_204)
+            .expect(HttpStatus.NO_CONTENT_204);
     });
 
     it('should send email with correct recovery code', async () => {
@@ -86,12 +103,13 @@ describe('testing password recovery', () => {
         // const isPlaying = await emailAdapter.sendEmail('a', 'b', 'c');
 
         if (!user) throw new Error('test cannot be performed.');
-        const userDB: UserDocument | null = await usersRepository.findUserByLoginOrEmail(user.email);
+        const userDB: UserDocument | null =
+            await usersRepository.findUserByLoginOrEmail(user.email);
         if (!userDB) throw new Error('test cannot be performed.');
 
         const data = {
-            'email': 'email123@mail.com'
-        }
+            email: 'email123@mail.com',
+        };
 
         await request(httpServer)
             .post(`${RouterPaths.auth}/password-recovery`)
@@ -102,15 +120,19 @@ describe('testing password recovery', () => {
         // expect(isPlaying).toBe(true);
 
         // Response получен, теперь проверим, что код сохранился верный.
-        const userWithCreatedPasswordRecoveryCode: UserDocument | null = await usersRepository.findUserByLoginOrEmail(data.email);
+        const userWithCreatedPasswordRecoveryCode: UserDocument | null =
+            await usersRepository.findUserByLoginOrEmail(data.email);
 
-        if (!userWithCreatedPasswordRecoveryCode) throw new Error('test cannot be performed.');
+        if (!userWithCreatedPasswordRecoveryCode)
+            throw new Error('test cannot be performed.');
 
         // Заодно сохраним код для последующих тестов
         passwordRecovery = userWithCreatedPasswordRecoveryCode.passwordRecovery;
 
         // проверим код на корректность
-        const restoredUserDb_id = await jwtService.getUserIdByToken(passwordRecovery.passwordRecoveryCode);
+        const restoredUserDb_id = await jwtService.getUserIdByToken(
+            passwordRecovery.passwordRecoveryCode,
+        );
         expect(userDB._id).toEqual(restoredUserDb_id);
         expect(passwordRecovery.active).toEqual(false);
     });
@@ -119,74 +141,79 @@ describe('testing password recovery', () => {
         if (!passwordRecovery) throw new Error('test cannot be performed.');
 
         const data = {
-            'newPassword': 'short',
-            'recoveryCode': passwordRecovery.passwordRecoveryCode
-        }
+            newPassword: 'short',
+            recoveryCode: passwordRecovery.passwordRecoveryCode,
+        };
 
         const response = await request(httpServer)
             .post(`${RouterPaths.auth}/new-password`)
             .send(data)
-            .expect(HttpStatus.BAD_REQUEST_400)
-        expect(response.body).toEqual({errorsMessages: [
-                { message: expect.any(String), field: 'newPassword' }
-            ]});
+            .expect(HttpStatus.BAD_REQUEST_400);
+        expect(response.body).toEqual({
+            errorsMessages: [
+                { message: expect.any(String), field: 'newPassword' },
+            ],
+        });
     });
 
     it('should return error if recoveryCode is incorrect; status 400;', async () => {
         const data = {
-            'newPassword': newPassword,
-            'recoveryCode': 'wrong recovery code'
-        }
+            newPassword: newPassword,
+            recoveryCode: 'wrong recovery code',
+        };
 
         const response = await request(httpServer)
             .post(`${RouterPaths.auth}/new-password`)
             .send(data)
-            .expect(HttpStatus.BAD_REQUEST_400)
-        expect(response.body).toEqual({errorsMessages: [
-                { message: expect.any(String), field: 'recoveryCode' }
-            ]});
+            .expect(HttpStatus.BAD_REQUEST_400);
+        expect(response.body).toEqual({
+            errorsMessages: [
+                { message: expect.any(String), field: 'recoveryCode' },
+            ],
+        });
     });
-
 
     it('should confirm password recovery; status 204;', async () => {
         if (!passwordRecovery) throw new Error('test cannot be performed.');
 
         const data = {
-            'newPassword': newPassword,
-            'recoveryCode': passwordRecovery.passwordRecoveryCode
-        }
+            newPassword: newPassword,
+            recoveryCode: passwordRecovery.passwordRecoveryCode,
+        };
 
         await request(httpServer)
             .post(`${RouterPaths.auth}/new-password`)
             .send(data)
-            .expect(HttpStatus.NO_CONTENT_204)
+            .expect(HttpStatus.NO_CONTENT_204);
     });
 
     it('should return status 401 if try to login with old password; status 401;', async () => {
         const data = {
-            'loginOrEmail': email,
-            'password': passwordBeforeChanging
-        }
+            loginOrEmail: email,
+            password: passwordBeforeChanging,
+        };
 
         await request(httpServer)
             .post(`${RouterPaths.auth}/login`)
             .send(data)
-            .expect(HttpStatus.UNAUTHORIZED_401)
+            .expect(HttpStatus.UNAUTHORIZED_401);
     });
 
     it('should sign in user with new password; status 200; content: JWT token;', async () => {
         const data = {
-            'loginOrEmail': email,
-            'password': newPassword
-        }
+            loginOrEmail: email,
+            password: newPassword,
+        };
 
         const response = await request(httpServer)
             .post(`${RouterPaths.auth}/login`)
             .send(data)
             .expect(HttpStatus.OK_200);
-        expect(response.body.accessToken).toMatch(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/);
+        expect(response.body.accessToken).toMatch(
+            /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/,
+        );
     });
-})
+});
 
 // todo:
 // замокать сервис почты (в джесте есть механизм Мокания данных)
