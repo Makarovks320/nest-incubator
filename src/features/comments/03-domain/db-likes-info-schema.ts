84990d7c52a7dced7a4a51180b1df7a071065dba
Kirill Makarov <kirill.makarov@visitech.ru>
2024-01-30 11:54:59 +0300

    fix: add schema for subdocument

--- src/features/comments/03-domain/db-likes-info-schema.ts
+++ src/features/comments/03-domain/db-likes-info-schema.ts
@@ -2,6 +2,19 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { LIKE_STATUS_DB_ENUM } from '../../likes/03-domain/types';
 
 @Schema()
+export class LikeInfo {
+    @Prop({ type: String, required: true })
+    userId: string;
+
+    @Prop({
+        type: Number,
+        enum: [LIKE_STATUS_DB_ENUM.LIKE, LIKE_STATUS_DB_ENUM.DISLIKE, LIKE_STATUS_DB_ENUM.NONE],
+        required: true,
+    })
+    likeStatus: number;
+}
+
+@Schema()
 class DbLikesInfo {
     @Prop({ required: true })
     likesCount: number;
@@ -9,15 +22,8 @@ class DbLikesInfo {
     @Prop({ required: true })
     dislikesCount: number;
 
-    @Prop({ required: true, default: [] })
-    likes: [
-        {
-            type: {
-                userId: { type: string; required: true };
-                likeStatus: { type: LIKE_STATUS_DB_ENUM; required: true };
-            };
-        },
-    ];
+    @Prop({ type: [LikeInfo], required: true, default: [] })
+    likes: LikeInfo[];
 }
 
 export const DbLikesInfoSchema = SchemaFactory.createForClass(DbLikesInfo);
